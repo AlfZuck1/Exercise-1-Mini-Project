d0c00e22d240a8dc396949b96cc26b84f71fc788
Alfredo Santiago Vázquez <164414681+AlfZuck@users.noreply.github.com>
2025-02-06 19:59:17 -0600

    FrontEnd
    FrontEnd Finished

--- user-manager-app/src/app/user.service.ts
+++ user-manager-app/src/app/user.service.ts
@@ -24,7 +24,6 @@ export class UserService {
 
   modifyUser(id: number, user: User): Observable<Object>{
     return this.httpClient.put(`${this.urlBase}/${id}`, user);
-
   }
 
   deleteUser(id: number): Observable<Object>{
